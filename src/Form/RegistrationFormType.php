0c269a54158420321d48d8c2a8a5fd63f32c6413
Lunamorgendorffer <nabdou68@gmail.com>
2023-05-30 13:43:52 +0200

    bootstrap

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -20,8 +20,8 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-        ->add('email',EmailType::class)
-        ->add('pseudo', TextType::class)
+        ->add('email',EmailType::class,['attr'=>['class'=>'form-control']] )
+        ->add('pseudo', TextType::class,['attr'=>['class'=>'form-control']])
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
@@ -38,6 +38,7 @@ class RegistrationFormType extends AbstractType
                 'required' => true,
                 'first_options'  => ['label' => 'Password'],
                 'second_options' => ['label' => 'Repeat Password'],
+                'attr'=>['class'=>'form-control']
                 
             ])
         ;
