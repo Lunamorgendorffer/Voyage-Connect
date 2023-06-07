f2c11e0a852015e958a6dbfe48078196ed4f3093
Lunamorgendorffer <nabdou68@gmail.com>
2023-06-07 17:01:41 +0200

    fonction edit, delete, show ..

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\EditProfileType;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -13,48 +15,41 @@ class UserController extends AbstractController
     #[Route('/user', name: 'app_user')]
     public function index(): Response
     {
-        $users = $entityManager->getRepository(User::class)->findAll();
 
-        return $this->render('post/index.html.twig', [
-            'posts' => $users,
-        ]);
+
+        return $this->render('user/index.html.twig');
     }
 
-    // // fonction ajout + edit une post
-    // #[Route('/post/add', name: 'add_post')]
-    // #[Route('/post/{id}/edit', name: 'edit_post')]
-    // public function add(EntityManagerInterface $entityManager, User $user = null, Request $request): Response 
-    // {
-    //     $user= $this->getUser(); 
-    //     if (!$user){ // si la user n'existe pas 
-    //         $user = new user();  // alors crée un nouvel objet user 
-    //     }
-    //     // on crée le formulaire 
-    //     $form = $this->createForm(UserType::class, $user);
-    //     $form->handleRequest($request);
+    // fonction ajout + edit une post
+    #[Route('/user/add', name: 'add_user')]
+    #[Route('/user/edit', name: 'edit_user')]
+    public function editUser(EntityManagerInterface $entityManager, Request $request): Response 
+    {
+        $user = $this->getUser();
+        // on crée le formulaire 
+        $form = $this->createForm(EditProfileType::class, $user);
+        $form->handleRequest($request);
 
-    //     //quand on sousmet le formulaire 
-    //     if($form->isSubmitted() && $form->isValid()){
-    //         // $images = $form->get('image')->getData();
-    //         $user->setUser($user);
-    //         $user->setCreationDate(new \DateTime());
+        //quand on sousmet le formulaire 
+        if($form->isSubmitted() && $form->isValid()){
+        
+            $user = $form->getData();
+            $entityManager->persist($user);// = prepare
+            $entityManager->flush();// execute, on envoie les données dans la db 
 
-    //         $user = $form->getData();
-    //         $entityManager->persist($user);// = prepare
-    //         $entityManager->flush();// execute, on envoie les données dans la db 
+            $this->addFlash('success','User updated');
 
-    //         return $this->redirectToRoute('app_user');
+            return $this->redirectToRoute('app_user');
 
-    //     }
+        }
 
-    //     // vue pour afficher le formulaire 
-    //     return $this->render('user/adduser.html.twig', [
-    //        'formAdduser' => $form->createView(),
-    //        'edit'=> $user->getId(),
+        // vue pour afficher le formulaire 
+        return $this->render('user/edit.html.twig', [
+           'form' => $form->createView(),
             
-    //     ]);
+        ]);
 
-    // }
+    }
 
     // fonction delete d'une user 
     #[Route('/user/{id}/delete', name: 'delete_user')]
