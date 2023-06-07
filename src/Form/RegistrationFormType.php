43341ae73ba89bb101914f72da0055e58b80b259
Lunamorgendorffer <nabdou68@gmail.com>
2023-06-07 18:35:08 +0200

    add avatar

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -5,10 +5,12 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\File;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
@@ -20,8 +22,25 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-        ->add('email',EmailType::class,['attr'=>['class'=>'input-field']] )
-        ->add('pseudo', TextType::class,['attr'=>['class'=>'input-field']])
+            ->add('email',EmailType::class,['attr'=>['class'=>'input-field']] )
+            ->add('pseudo', TextType::class,['attr'=>['class'=>'input-field']])
+            ->add('avatar', FileType::class, [
+                'label' => 'image',
+                'mapped' => false,
+                'required' => false,
+                'constraints' => [
+                    new File([
+                        'maxSize' => '1024K',
+                        'mimeTypes' => [
+                            'image/jpg',
+                            'image/png',
+                            'image/jpeg',
+                            'image/gif',
+                        ],
+                        'mimeTypesMessage' => 'Please upload an image',
+                    ])
+                ],
+            ])
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
