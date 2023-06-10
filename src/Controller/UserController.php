9b4cc5cb443c6248142db2a472b36fea78e4090a
Lunamorgendorffer <nabdou68@gmail.com>
2023-06-10 15:08:50 +0200

    editPassoword

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,11 +4,13 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\EditProfileType;
+use App\Form\EditPasswordType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserController extends AbstractController
 {
@@ -21,7 +23,6 @@ class UserController extends AbstractController
     }
 
     // fonction ajout + edit une post
-    #[Route('/user/add', name: 'add_user')]
     #[Route('/user/edit', name: 'edit_user')]
     public function editUser(EntityManagerInterface $entityManager, Request $request): Response 
     {
@@ -45,6 +46,50 @@ class UserController extends AbstractController
 
         // vue pour afficher le formulaire 
         return $this->render('user/edit.html.twig', [
+           'form' => $form->createView(),
+            
+        ]);
+
+    }
+
+    // fonction ajout + edit une post
+    #[Route('/user/editPassword/{id}', name: 'edit_password', methods:['GET', 'POST'])]
+    public function editPassword(User $user, EntityManagerInterface $entityManager, UserPasswordHasherInterface $hasher,Request $request): Response 
+    {
+        $form = $this->createForm(EditPasswordType::class);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            if ($hasher->isPasswordValid($user, $form->getData()['plainPassword'])) {
+                $user->setUpdatedAt(new \DateTimeImmutable());
+                $user->setPassword(
+                    $hasher->hashPassword($user,
+                    $form->getData()['newPassword']
+                    )
+                );
+
+                $this->addFlash(
+                    'success',
+                    'Le mot de passe a été modifié.'
+                );
+
+                $entityManager->persist($user);
+                $entityManager->flush();
+
+                return $this->redirectToRoute('app_user');
+            }else {
+                $this->addFlash(
+                    'warning',
+                    'Le mot de passe renseigné est incorrect.'
+                );
+            }
+
+        }
+
+        // vue pour afficher le formulaire 
+        return $this->render('user/editPassword.html.twig', [
            'form' => $form->createView(),
             
         ]);
