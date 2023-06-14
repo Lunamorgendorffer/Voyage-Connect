a79a2b282a84f06a94f0130e0fa0bc45736a22d9
Lunamorgendorffer <nabdou68@gmail.com>
2023-06-14 12:13:20 +0200

    update

--- config/bundles.php
+++ config/bundles.php
@@ -15,5 +15,4 @@ return [
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
-    Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
 ];
