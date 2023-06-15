c40d56cfdc3caaede81f782b0e91fa1e715373f0
Lunamorgendorffer <nabdou68@gmail.com>
2023-06-15 11:59:22 +0200

    update fonct like

--- src/Controller/LikeController.php
+++ src/Controller/LikeController.php
@@ -10,19 +10,24 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class LikeController extends AbstractController
 {
-    #[Route('/like', name: 'app_like')]
-    public function index(EntityManagerInterface $entityManager, Post $post = null): Response
+    #[Route('/like/post/{id}', name: 'app_like')]
+    public function index(EntityManagerInterface $entityManager, Post $post): Response
     {
         $user = $this->getUser();
         
-        if ($post->isLikeByUser($user)) {
+        if ($post->isLikedByUser($user)) {
 
             $post->removeLike($user);
             $$entityManager->flush();
+
+            return $this->json(['message' => 'Like has been deleted']);
         }
 
         $post->addLike($user);
+        
         $entityManager->flush();
+        
+        return $this->json(['message' => 'Like has been added']);
     }
 
     
