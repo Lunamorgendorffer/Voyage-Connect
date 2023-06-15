079119eb64551ccc9035c7133894c65f93d35316
Lunamorgendorffer <nabdou68@gmail.com>
2023-06-15 14:45:08 +0200

    update class Like

--- assets/scripts/like.js
+++ assets/scripts/like.js
@@ -1,28 +1,37 @@
 // import axios from 'axios';
 
 export default class Like {
-    constructor(likeElements) {
+  constructor(likeElements) {
       this.likeElements = likeElements;
       console.log(likeElements);
         if (this.likeElements) {
           this.init();
         }
-    }
+  }
 
-    init() {
+  init() {
         this.likeElements.map(element => {
           element.addEventListener('click', this.onClick)
         })
-    }
+  }
 
-    onClick(event){
-        console.log(event);
-        event.preventDefault();
-        const url = this.href;
+  onClick(event){
+    console.log(event);
+    event.preventDefault();
+    const url = this.href;
 
-        axios.get(url).then(res => {
-            console.log(res);
-        })
+    axios.get(url).then(res => {
+      console.log(res);
+
+      // get the buttons
+      const heartFilled = this.querySelector('.filled');
+      const emptyHeart = this.querySelector('.unfilled');
+  
+      // toggle the display of the filled and empty buttons
+      heartFilled.classList.toggle('d-none');
+      emptyHeart.classList.toggle('d-none');
+         
+    })
         
-    }
+  }
 }
