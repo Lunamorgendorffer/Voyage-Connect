ec653543a318686c93a28e127321190bdd3e25f4
Lunamorgendorffer <nabdou68@gmail.com>
2023-06-28 11:59:32 +0200

    start dql trend user

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -56,6 +56,52 @@ class UserRepository extends ServiceEntityRepository implements PasswordUpgrader
         $this->save($user, true);
     }
 
+
+    public function getTrendUsers(){
+
+        /*$queryBuilder = $this->createQueryBuilder('p')
+            ->select('p')
+            ->innerJoin('p.user', 'u');
+
+            if (!empty($searchData->q)) {
+                $queryBuilder
+                    ->andWhere('p.title LIKE :q')
+                    ->orWhere('u.pseudo LIKE :q')
+                    ->setParameter('q', "%{$searchData->q}%");
+            }
+
+        return $queryBuilder->getQuery()->getResult(); */
+
+        
+
+        $query = $entityManager->createQuery('
+        SELECT DISTINCT u.pseudo, p.title
+        FROM App\Entity\Post p 
+        INNER JOIN p.likes l
+        INNER JOIN App\Entity\User u
+        WHERE p.id IN (
+            SELECT p2.id
+            FROM App\Entity\Post p2
+            INNER JOIN p2.likes l2
+            GROUP BY p2.id
+            HAVING COUNT(l2) >= 2
+        )')
+        ->select('u.pseudo, p.title')
+        ->from ('App\Entity\Post', 'p')
+        ->innerJoin('p.likes', 'l')
+        ->innerJoin('App\Entity\User', 'u')
+        ->inWhere('p.id','')
+        ;
+    
+    $results = $query->getResult();
+    
+
+        
+
+
+
+    }
+
 //    /**
 //     * @return User[] Returns an array of User objects
 //     */
