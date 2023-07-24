76776a93392cecd21c37790152a3b0998b5bbe17
Lunamorgendorffer <nabdou68@gmail.com>
2023-07-24 18:40:34 +0200

    Injection API

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,15 +2,34 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Service\CallApiService;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
-    public function index(): Response
+    private $callApiService;
+
+    public function __construct(CallApiService $callApiService)
     {
-        return $this->render('home/index.html.twig', []);
+        $this->callApiService = $callApiService;
+    }
+    
+    #[Route('/', name: 'app_home')]
+    public function index(CallApiService $restCountriesService): Response
+    {
+        // Liste des régions pour afficher les liens vers les détails de chaque région
+        $regions = [
+        "Asia" => "img/b.jpg",
+        "Americas" => "img/am.jpg" , 
+        "Africa" => "img/a.jpg",
+        "Europe"=> "img/home-mountain.jpg" ,
+        "Oceania" => "img/home-beach.jpg"];
+
+
+        return $this->render('home/index.html.twig', [
+            'regions' => $regions,
+        ]);
     }
 }
