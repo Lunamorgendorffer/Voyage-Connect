95c8c4cc96826c62b622b7ba583a109c44ed0c22
Lunamorgendorffer <nabdou68@gmail.com>
2023-07-27 12:52:41 +0200

    update api style

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -15,13 +15,12 @@ class ApiController extends AbstractController
     public function index(CallApiService $restCountriesService): Response
     {
         // Liste des régions pour afficher les liens vers les détails de chaque région
-        $regions = [
-            "Asia" => "img/b.jpg",
-            "Americas" => "img/am.jpg" , 
-            "Africa" => "img/a.jpg",
-            "Europe"=> "img/home-mountain.jpg" ,
-            "Oceania" => "img/home-beach.jpg"
-        ];
+       $regions = [
+        "Asia" => "img/b.jpg",
+        "Americas" => "img/am.jpg" , 
+        "Africa" => "img/a.jpg",
+        "Europe"=> "img/home-mountain.jpg" ,
+        "Oceania" => "img/home-beach.jpg"];
 
         return $this->render('api/index.html.twig', [
             'regions' => $regions,
