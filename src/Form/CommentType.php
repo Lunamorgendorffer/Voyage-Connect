babec3f30b76fb06a9a74c28e67414ac9c85b181
Lunamorgendorffer <nabdou68@gmail.com>
2023-08-17 19:25:36 +0200

    Update style comments

--- src/Form/CommentType.php
+++ src/Form/CommentType.php
@@ -6,17 +6,27 @@ use App\Entity\Comment;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class CommentType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            // ->add('creationDate')
-            ->add('text')
-            // ->add('post',)
-            // // ->add('user')
-            // ->add('usersLike')
+        
+            ->add('text', TextareaType::class,[
+                    'label' => 'Your Comment',
+                
+                    'attr'=>[
+                        'class'=>'form-control',
+                        'placeholder' => 'Please write your comment here...'
+                    ]
+                ])
+            ->add('submit', SubmitType::class,[ // Champ de téléchargement de l'image du post
+                'label' => 'Send',
+                'attr'=>['class'=>'create-account']]) // Bouton de soumission du formulaire
+        
         ;
     }
 
